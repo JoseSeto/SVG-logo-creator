d8fed3c6d202baaad484920a168cb6b4de52640a
Jose Seto <joseseto8@gmail.com>
2023-05-25 08:05:14 -0700

    Added require lines and inquirer quetions

---
+++ index.js
@@ -0,0 +1,48 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+const {Trianlge, Square, Circle } = require("./lib/shapes")
+
+function shapeQuestions () {
+    inquirer
+    .prompt([
+      // Text prompt
+      {
+        type: "input",
+        message:
+          "What text would you like you logo to display? (Enter up to three characters)",
+        name: "text",
+      },
+      // Text color prompt
+      {
+        type: "input",
+        message:
+          "Choose text color (Enter color keyword OR a hexadecimal number)",
+        name: "textColor",
+      },
+      // Shape choice prompt
+      {
+        type: "list",
+        message: "What shape would you like the logo to render?",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      // Shape color prompt
+      {
+        type: "input",
+        message:
+          "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+        name: "shapeBackgroundColor",
+      },
+    ])
+    
+    .then((answers) => {
+      // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptUser();
+      } else {
+        // Calling write file function to generate SVG file
+        writeToFile("logo.svg", answers);
+      }
+    });
+}
