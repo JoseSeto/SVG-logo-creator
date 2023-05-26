f1e1d195f9684f134d5e0e2a4583356bef32ec6c
Jose Seto <joseseto8@gmail.com>
2023-05-26 10:01:28 -0700

    added write to file function

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
-const {Trianlge, Square, Circle } = require("./lib/shapes")
+const {Trianlge, Square, Circle, Triangle } = require("./lib/shapes")
+
+
+function writeToFile(filename, asnwers){
+  let string = "";
+  string = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  sting += "<g>"
+  string += `${answers.shape}`
+
+  let userShape;
+  if (answers.shape === "triangle"){
+    userShape = new Triangle()
+    string += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`
+  }
+  else if (answers.shape === "sqaure"){
+    userShape = new Square()
+    string += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`
+  }
+  else {
+    userShape = new Circle()
+    string += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`
+  }
+
+  string += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  string += "<g>";
+  string += "</svg>";
+
+  fs.writeFile(filename, string, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg")
+  })
+}
+
 
 function shapeQuestions () {
     inquirer
@@ -46,3 +77,4 @@ function shapeQuestions () {
       }
     });
 }
+shapeQuestions();
