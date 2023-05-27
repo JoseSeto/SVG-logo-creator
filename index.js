2fb437c2927c0df0b532d26828335e3b5cba3d09
Jose Seto <joseseto8@gmail.com>
2023-05-26 18:45:14 -0700

    fixed typos

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const fs = require("fs")
 const {Trianlge, Square, Circle, Triangle } = require("./lib/shapes")
 
 
-function writeToFile(filename, asnwers){
+function writeToFile(filename, answers){
   let string = "";
   string = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-  sting += "<g>"
+  string += "<g>"
   string += `${answers.shape}`
 
   let userShape;
